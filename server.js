b5da3f066ddbfcbc5a685ea37db22a68e0975528
Bryan <bryan.fennell@hotmail.com>
2020-05-12 19:30:12 -0400

    Removed unneeded consol.log(). Extracted catch callback into its own function.

--- server.js
+++ server.js
@@ -23,22 +23,20 @@ app.set("view engine", "handlebars")
   Show the lists of burgers on the main page.
  */
 app.get("/", function(req, res) {
-  burgers.getAll().then((result, error) => {
-    const
-      eaten = result.filter(b => b.eaten),
-      uneaten = result.filter(b => !b.eaten)
+  burgers.getAll()
+    .then((result, error) => {
+      const
+        eaten = result.filter(b => b.eaten),
+        uneaten = result.filter(b => !b.eaten)
 
-    if (error) {
-      throw new Error(error)
-    }
-  
-    res.render("index", {
-      eaten: eaten,
-      uneaten: uneaten
+      if (error) throw new Error(error)
+    
+      res.render("index", {
+        eaten: eaten,
+        uneaten: uneaten
+      })
     })
-  }).catch((error) => {
-    console.log(error)
-  })
+    .catch(logError)
 })
 
 
@@ -51,35 +49,41 @@ app.post("/", function(req, res) {
   if (!burger.trim()) {
     res.redirect("/")
   } else {
-    burgers.add(burger).then((result, error) => {
-      if (error) {
-        throw new Error(error)
-      }
-      res.redirect("/")
-    }).catch((error) => {
-      console.log(error)
-    })
+    burgers.add(burger)
+      .then((result, error) => {
+        if (error) throw new Error(error)
+        res.redirect("/")
+      })
+      .catch(logError)
   }
 })
 
 
 /*
-  Mark a burger as having been eaten.
+  Consume a burger.
  */
 app.post("/devour", function(req, res) {
   const { burgerId } = req.body
 
-  burgers.devour(burgerId).then((result, error) => {
-    if (error) {
-      throw new Error(error)
-    }
-    res.redirect("/")
-  }).catch((error) => {
-    console.log(error)
-  })
+  burgers.devour(burgerId)
+    .then((result, error) => {
+      if (error) throw new Error(error)
+      res.redirect("/")
+    })
+    .catch(logError)
 })
 
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT)
 })
+
+
+// --- Utility function ---
+
+/*
+  Log any errors to the console.
+ */
+function logError(error) {
+  console.error(error)
+}
