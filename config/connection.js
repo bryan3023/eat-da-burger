a0eb5085245dee34ce1480c0ee64058f3b239913
Bryan <bryan.fennell@hotmail.com>
2020-05-20 21:50:55 -0400

    Updated MySQL connection to use config.json

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,16 @@ module.exports = function() {
   if (process.env.JAWSDB_URL) {
     return mysql.createConnection(process.env.JAWSDB_URL)
   } else {
+    const
+      env = process.env.NODE_ENV || 'development',
+      config = require('./config.json')[env]
+  
     return mysql.createConnection({
-      host: "localhost",
-      port: 3306,
-      user: "bryan3023.eat_da_burger",
-      password: "ifM91qUy89*#%R^QsFjN",
-      database: "eat_da_burger_DB"
+      host: config.host,
+      port: config.port,
+      user: config.username,
+      password: config.password,
+      database: config.database
     })
   }
 }
